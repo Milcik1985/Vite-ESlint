660119b648e25e569111c416b9d04c00ae4d0a52
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-03-06 10:41:48 +0200

    ESlint&Vite config

---
+++ public/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1. Susikurt savo VITE TS aplinką.
+// 2. Užmesti akį į VITE dokumentaciją, pabandyt ją pasiskaityt;
+// 3. Ištestuot ar ts failai kompiliuojas i js; // reiks paleist buildinimosi komenda;
+// 4. Paleisti projektą bei patikrinti ar norimi rezultatai atvaizduojami ekrane;
+// 5. Prie savo kodo pridėt eslint naudojantis eslint dokumentacija;
+// 6. Pratestuot ar eslint  klaidų radimas veikia;
+// 7. Pridėt eslint komandą kuri patikrins visą kodą bei ją paleist.
+// 8. Dadėt prettier konfiguracija, padaryt;
+// 9. Pratestuot ar veikia formatavimas;
+// 10. Pabandyt pratestuot buvusių paskaitų kodą VITE aplinkoje;
